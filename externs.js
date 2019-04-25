0b5dc4e50218e26aaeade2a152c57e0909fcc3d6
Anton <anton@adc.sh>
2019-04-25 10:34:54 +0300

    types

--- externs.js
+++ externs.js
@@ -1,1 +1,8 @@
 /* typal types/index.xml */
+/** @const */
+var _indicatrix = {}
+/**
+ * The optional options for the indicator, such as the refresh interval.
+ * @typedef {{ interval: (number|undefined), writable: (!(NodeJS.WriteStream|stream.Writable)|undefined) }}
+ */
+_indicatrix.Options
